e5e8fae907afe4c4ad386c6e3e2caf3837a103c4
Huzaifa Aslam <huzaifaslam45@gmail.com>
2021-04-13 23:46:20 +0500

    BISMILLAH

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+  constructor() {
+    this.todos = [
+      {
+        sno: 1,
+        title: 'title 1',
+        desc: 'description',
+        active: true,
+      },
+      {
+        sno: 2,
+        title: 'title 2',
+        desc: 'description',
+        active: true,
+      },
+      {
+        sno: 3,
+        title: 'title 3',
+        desc: 'description',
+        active: true,
+      },
+    ];
+  }
+
+  ngOnInit(): void {}
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'old heading';
+  constructor() {
+    setTimeout(() => {
+      this.title = 'new heading';
+    }, 4000);
+  }
 }
